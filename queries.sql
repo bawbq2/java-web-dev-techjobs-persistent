b8d216bb9f95e581a9d4e8cf717051c9ef145110
Barbi Semar Ryan Semar <semar@Barbis-MacBook-Air.local>
2020-07-08 20:25:39 -0500

    added sql quesries to queries.sql

--- queries.sql
+++ queries.sql
@@ -1,7 +1,19 @@
 ## Part 1: Test it with SQL
 
+SHOW fields FROM job;
+
 ## Part 2: Test it with SQL
+
+SELECT name
+FROM employer
+WHERE location = "St. Louis";
 
 ## Part 3: Test it with SQL
 
+DROP TABLE job;
+
 ## Part 4: Test it with SQL
+
+SELECT name, description FROM skill
+WHERE skill.id IN(SELECT skill.id FROM job_skills WHERE skills_id is not null)
+ORDER BY name ASC;
